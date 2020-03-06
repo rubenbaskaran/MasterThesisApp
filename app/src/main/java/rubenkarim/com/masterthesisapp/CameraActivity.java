2d6b1dc8b1b3363d27cb14ec8e9dfc95be93f6da
Karim <Karmo>
2020-03-06 14:46:01 +0100

    add modules to project

--- app/src/main/java/rubenkarim/com/masterthesisapp/CameraActivity.java
+++ app/src/main/java/rubenkarim/com/masterthesisapp/CameraActivity.java
@@ -11,6 +11,9 @@ public class CameraActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_camera);
+
+
     }
 }
