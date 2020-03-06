6b051648d18bac364acbac4c686af056c53c8c9e
Ruben Baskaran <rbaskaran92@gmail.com>
2020-03-06 12:39:25 +0100

    added a todo

--- app/src/main/java/rubenkarim/com/masterthesisapp/MainActivity.java
+++ app/src/main/java/rubenkarim/com/masterthesisapp/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //TODO: MAKE ME A SANDWICH
+        //TODO: This is a test
     }
 }
