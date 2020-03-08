0537e81587955d3dc07357df9d01e246c1790706
Ruben Baskaran <rbaskaran92@gmail.com>
2020-03-08 17:23:09 +0100

    added code for showing drawable in imageview

--- app/src/main/java/rubenkarim/com/masterthesisapp/Activities/OverviewActivity.java
+++ app/src/main/java/rubenkarim/com/masterthesisapp/Activities/OverviewActivity.java
@@ -1,17 +1,31 @@
 package rubenkarim.com.masterthesisapp.Activities;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import rubenkarim.com.masterthesisapp.R;
 
-import android.os.Bundle;
-
 public class OverviewActivity extends AppCompatActivity
 {
+    ImageView imageView_patientImage;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
+        imageView_patientImage = findViewById(R.id.imageView_patientImage);
+        SetPicture("test_picture");
+    }
+
+    private void SetPicture(String filename)
+    {
+        Resources resources = getApplicationContext().getResources();
+        int resourceId = resources.getIdentifier(filename, "drawable", getApplicationContext().getPackageName());
+        imageView_patientImage.setImageResource(resourceId);
     }
 }
