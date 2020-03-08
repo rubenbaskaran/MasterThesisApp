34061fd043891f7659246ce5d228a6b07c734a97
Ruben Baskaran <rbaskaran92@gmail.com>
2020-03-08 18:08:10 +0100

    added imageview + navigation buttons + setpicture method + passing extras between activities

--- app/src/main/java/rubenkarim/com/masterthesisapp/Activities/MarkerActivity.java
+++ app/src/main/java/rubenkarim/com/masterthesisapp/Activities/MarkerActivity.java
@@ -1,17 +1,46 @@
 package rubenkarim.com.masterthesisapp.Activities;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import rubenkarim.com.masterthesisapp.R;
 
-import android.os.Bundle;
-
 public class MarkerActivity extends AppCompatActivity
 {
+    ImageView imageView_markerImage;
+    String filename = "default_picture";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_marker);
+        imageView_markerImage = findViewById(R.id.imageView_markerImage);
+
+        Intent receivedIntent = getIntent();
+        if (receivedIntent.hasExtra("filename"))
+        {
+            filename = receivedIntent.getStringExtra("filename");
+        }
+
+        SetPicture();
+    }
+
+    private void SetPicture()
+    {
+        Resources resources = getApplicationContext().getResources();
+        int resourceId = resources.getIdentifier(filename, "drawable", getApplicationContext().getPackageName());
+        imageView_markerImage.setImageResource(resourceId);
+    }
+
+    public void SubmitOnClick(View view)
+    {
+        Intent intent = new Intent(getApplicationContext(), OverviewActivity.class);
+        intent.putExtra("filename", filename);
+        startActivity(intent);
     }
 }
--- app/src/main/java/rubenkarim/com/masterthesisapp/Activities/OverviewActivity.java
+++ app/src/main/java/rubenkarim/com/masterthesisapp/Activities/OverviewActivity.java
@@ -1,8 +1,9 @@
 package rubenkarim.com.masterthesisapp.Activities;
 
+import android.content.Intent;
 import android.content.res.Resources;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,21 +12,41 @@ import rubenkarim.com.masterthesisapp.R;
 public class OverviewActivity extends AppCompatActivity
 {
     ImageView imageView_patientImage;
+    String filename = "default_picture";
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
         imageView_patientImage = findViewById(R.id.imageView_patientImage);
-        SetPicture("test_picture");
+
+        Intent receivedIntent = getIntent();
+        if (receivedIntent.hasExtra("filename"))
+        {
+            filename = receivedIntent.getStringExtra("filename");
+        }
+
+        SetPicture();
     }
 
-    private void SetPicture(String filename)
+    private void SetPicture()
     {
         Resources resources = getApplicationContext().getResources();
         int resourceId = resources.getIdentifier(filename, "drawable", getApplicationContext().getPackageName());
         imageView_patientImage.setImageResource(resourceId);
+    }
+
+    public void BackOnClick(View view)
+    {
+        Intent intent = new Intent(getApplicationContext(), MarkerActivity.class);
+        intent.putExtra("filename", filename);
+        startActivity(intent);
+    }
+
+    public void SaveOnClick(View view)
+    {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 }
