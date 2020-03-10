e1505182ac726c80c28c2685d99fb0cf83894e72
Karim <Karmo>
2020-03-10 21:57:31 +0100

    is now saving to device!

--- app/src/main/java/rubenkarim/com/masterthesisapp/Activities/CameraActivity.java
+++ app/src/main/java/rubenkarim/com/masterthesisapp/Activities/CameraActivity.java
@@ -1,10 +1,16 @@
 package rubenkarim.com.masterthesisapp.Activities;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 
@@ -12,6 +18,8 @@ import com.google.android.material.snackbar.Snackbar;
 import com.google.common.util.concurrent.ListenableFuture;
 
 import java.io.File;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 
@@ -111,27 +119,32 @@ public class CameraActivity extends AppCompatActivity{
     }
 
     public void TakePictureOnClick(View view) {
-            String path = String.valueOf(getBaseContext().getFilesDir());
-        cameraViewFinder.takePicture(
 
-                (imageSaver)-> {
-                    Log.d(TAG, "TakePictureOnClick: " + path);
-                    imageSaver.run();},
-                new ImageCapture.OnImageCapturedCallback() {
-                    @Override
-                    public void onCaptureSuccess(@NonNull ImageProxy image) {
-                        super.onCaptureSuccess(image);
-                        Log.d(TAG, "onCaptureSuccess: " +image.getImageInfo());
-                    }
+        File mImageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "/Masterthesisimages/");
+        boolean isDirectoryCreated = mImageDir.exists() || mImageDir.mkdirs();
 
-                    @Override
-                    public void onError(@NonNull ImageCaptureException exception) {
-                    Log.d(TAG, "ERROR: " + exception);
-            }
+        if(isDirectoryCreated){
+
+            String fileName = new SimpleDateFormat("HH:mm:ss").format(new Timestamp(System.currentTimeMillis()));
+            File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/Masterthesisimages", fileName+".jpg");
+
+            cameraViewFinder.takePicture(file, Runnable::run, new ImageCapture.OnImageSavedCallback() {
+                @Override
+                public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                    Bundle params = new Bundle();
+                    Log.i(TAG, "onImageSaved: Picture saved! path: " + file.getPath());
+                    params.putString("FILE_PATH", file.getPath());
+
+                }
+
+                @Override
+                public void onError(@NonNull ImageCaptureException exception) {
+                    Log.d(TAG, "onError: " + exception);
+                }
+            });
+
+        } else {
+            Log.e(TAG, "TakePictureOnClick: There is an error with creating dir!");
         }
-        );
-
-        //Intent intent = new Intent(getApplicationContext(), MarkerActivity.class);
-        //startActivity(intent);
     }
 }
