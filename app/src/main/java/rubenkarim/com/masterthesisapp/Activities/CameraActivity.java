d847c0a8c7a3d5d6a8606aab3129a527bf3798a0
Karim <Karmo>
2020-03-10 22:02:25 +0100

    cleaning up code

--- app/src/main/java/rubenkarim/com/masterthesisapp/Activities/CameraActivity.java
+++ app/src/main/java/rubenkarim/com/masterthesisapp/Activities/CameraActivity.java
@@ -1,44 +1,25 @@
 package rubenkarim.com.masterthesisapp.Activities;
 
 import android.Manifest;
-import android.app.Activity;
-import android.content.ContentResolver;
-import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.media.Image;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 
 import com.google.android.material.snackbar.Snackbar;
-import com.google.common.util.concurrent.ListenableFuture;
 
 import java.io.File;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.camera2.Camera2Config;
-import androidx.camera.core.CameraSelector;
-import androidx.camera.core.CameraXConfig;
 import androidx.camera.core.ImageCapture;
 import androidx.camera.core.ImageCaptureException;
-import androidx.camera.core.ImageProxy;
-import androidx.camera.core.Preview;
-import androidx.camera.core.Preview.Builder;
-import androidx.camera.core.impl.ImageCaptureConfig;
-import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.CameraView;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.lifecycle.LifecycleOwner;
 import rubenkarim.com.masterthesisapp.R;
 
 public class CameraActivity extends AppCompatActivity{
@@ -52,6 +33,7 @@ public class CameraActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
         rootView = findViewById(R.id.linearLayout_CameraActivity);
+
         //Check Permissions:
         if (!checkCameraPermissions()) {
             requestCameraPermissions();
@@ -71,28 +53,20 @@ public class CameraActivity extends AppCompatActivity{
 
     private boolean checkCameraPermissions() {
         int permissionState = ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
-        // Check if the Camera permission is already available.
         if (permissionState != PackageManager.PERMISSION_GRANTED) {
-            // Camera permission has not been granted.
             Log.i(TAG, "CAMERA permission has NOT been granted.");
             return false;
         } else {
-            // Camera permissions are available.
             Log.i(TAG, "CAMERA permission has already been granted.");
             return true;
         }
     }
 
     private void requestCameraPermissions() {
-        // Provide an additional rationale to the user. This would happen if the user denied the
-        // request previously, but didn't check the "Don't ask again" checkbox.
         if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
-            Log.i(TAG, "Displaying camera permission rationale to provide additional context.");
             Snackbar.make(rootView, R.string.camera_permission_rationale, Snackbar
                     .LENGTH_INDEFINITE)
                     .setAction(R.string.ok, new View.OnClickListener() {
-
-
                         @Override
                         public void onClick(View view) {
                             // Request Camera permission
@@ -103,10 +77,6 @@ public class CameraActivity extends AppCompatActivity{
                     })
                     .show();
         } else {
-            Log.i(TAG, "Requesting camera permission");
-            // Request Camera permission. It's possible this can be auto answered if device policy
-            // sets the permission in a given state or the user denied the permission
-            // previously and checked "Never ask again".
             ActivityCompat.requestPermissions(CameraActivity.this,
                     new String[]{Manifest.permission.CAMERA},
                     REQUEST_PERMISSIONS_REQUEST_CODE);
@@ -131,10 +101,10 @@ public class CameraActivity extends AppCompatActivity{
             cameraViewFinder.takePicture(file, Runnable::run, new ImageCapture.OnImageSavedCallback() {
                 @Override
                 public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
-                    Bundle params = new Bundle();
                     Log.i(TAG, "onImageSaved: Picture saved! path: " + file.getPath());
-                    params.putString("FILE_PATH", file.getPath());
-
+                    Intent intent = new Intent(getApplicationContext(), MarkerActivity.class);
+                    intent.putExtra("filename", file.getPath());
+                    startActivity(intent);
                 }
 
                 @Override
