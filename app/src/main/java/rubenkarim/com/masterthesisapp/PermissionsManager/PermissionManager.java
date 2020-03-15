629a52c9ad00a2653ff55d5b8b45f843a8194a86
Karim <Karmo>
2020-03-15 19:27:08 +0100

    add java doc

--- app/src/main/java/rubenkarim/com/masterthesisapp/PermissionsManager/PermissionManager.java
+++ app/src/main/java/rubenkarim/com/masterthesisapp/PermissionsManager/PermissionManager.java
@@ -17,6 +17,12 @@ public class PermissionManager implements ActivityCompat.OnRequestPermissionsRes
     private PermissionListener permissionListener;
     private final int PERMISSIONS_REQUEST_CODE = 10;
 
+    /**
+     * Is used to check whether a permission has been granted
+     * @param context The activity context
+     * @param permissionTypes the permission that will be checked
+     * @return True if all permissions is already granted or False if one permissions is denied
+     */
     public static boolean checkPermissions(Context context, String... permissionTypes){
         ArrayList<Integer> permissionsStats = new ArrayList<>();
         for (String permissionType: permissionTypes) {
@@ -33,8 +39,13 @@ public class PermissionManager implements ActivityCompat.OnRequestPermissionsRes
         return true;
     }
 
-
-
+    /**
+     * Request permissions of the user.
+     * @param activity the Activity in which the instance is created.
+     * @param permissionListener An interface which handes the results from the user
+     * @param permissions The permissions that the user will be asked for, just input as many permission that you like
+     *                    the android OS will take care of not asking for already granted permission.
+     */
     public void requestPermissions(Activity activity, PermissionListener permissionListener, String... permissions){
         Log.i(TAG, "requestPermissions for " + Arrays.toString(permissions));
         this.permissionListener = permissionListener;
@@ -45,7 +56,14 @@ public class PermissionManager implements ActivityCompat.OnRequestPermissionsRes
             );
     }
 
-
+    /**
+     * Is used to tap into the Activity's callback.
+     * simply call this method in the activity's {@link Activity#onRequestPermissionsResult(int, String[], int[])} in which this class is instantiated.
+     * The parameters are the same...
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         Log.i(TAG, "onRequestPermissionsResult: HELLO " + requestCode);
