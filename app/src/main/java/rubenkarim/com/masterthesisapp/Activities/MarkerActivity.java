0f6653887d06238da9a93a9233e4a6e2265f9c36
Ruben Baskaran <rbaskaran92@gmail.com>
2020-03-22 21:44:16 +0100

    refactored markeractivity and added todos for sprint 3

--- app/src/main/java/rubenkarim/com/masterthesisapp/Activities/MarkerActivity.java
+++ app/src/main/java/rubenkarim/com/masterthesisapp/Activities/MarkerActivity.java
@@ -27,27 +27,23 @@ import rubenkarim.com.masterthesisapp.R;
 import rubenkarim.com.masterthesisapp.Utilities.ImageProcessing;
 import rubenkarim.com.masterthesisapp.Utilities.Scaling;
 
-// TODO: Refactor and optimize
 public class MarkerActivity extends AppCompatActivity {
-    ImageView imageView_markerImage;
-    String filename = "default_picture";
-    Boolean isThermalPicture = false;
-    private static final String TAG = CameraActivity.class.getSimpleName();
-    String eyeMarkerPath = "android.resource://rubenkarim.com.masterthesisapp/drawable/" + "eye_marker";
-    String noseMarkerPath = "android.resource://rubenkarim.com.masterthesisapp/drawable/" + "nose_marker";
-    int imageViewVerticalOffset;
-    int imageHeight;
-    int imageWidth;
-    int markerWidthHeight = 200;
-    View container;
-    GradientModel gradientAndPositions;
+
+    //region Properties
+    private String filename;
+    private Boolean isThermalPicture = false;
+    private int imageViewVerticalOffset;
+    private int imageHeight;
+    private int imageWidth;
+    private ImageView imageView_markerImage;
+    private GradientModel gradientAndPositions;
+    //endregion
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_marker);
         imageView_markerImage = findViewById(R.id.imageView_markerImage);
-        container = findViewById(R.id.linearLayout_MarkerActivity);
 
         Intent receivedIntent = getIntent();
         if (receivedIntent.hasExtra("filename")) {
@@ -74,35 +70,6 @@ public class MarkerActivity extends AppCompatActivity {
         setPicture();
     }
 
-    @SuppressLint("ClickableViewAccessibility")
-    private void SetOnTouchListener(ImageView img) {
-        img.setOnTouchListener(new View.OnTouchListener() {
-            PointF DownPT = new PointF(); // Record Mouse Position When Pressed Down
-            PointF StartPT = new PointF(); // Record Start Position of 'img'
-
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_MOVE:
-                        img.setX((int) (StartPT.x + event.getX() - DownPT.x));
-                        img.setY((int) (StartPT.y + event.getY() - DownPT.y));
-                        StartPT.set(img.getX(), img.getY());
-                        break;
-                    case MotionEvent.ACTION_DOWN:
-                        DownPT.set(event.getX(), event.getY());
-                        StartPT.set(img.getX(), img.getY());
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        getCoordinates(img);
-                        break;
-                    default:
-                        break;
-                }
-                return true;
-            }
-        });
-    }
-
     private void setPicture() {
         ImageProcessing.FixImageOrientation(filename);
         int[] imageOriginalDimensions = null;
@@ -111,19 +78,17 @@ public class MarkerActivity extends AppCompatActivity {
             try {
                 if (ThermalImageFile.isThermalImage(filename)) {
                     ThermalImageFile thermalImageFile = (ThermalImageFile) ImageFactory.createImage(filename);
-
                     JavaImageBuffer javaBuffer = thermalImageFile.getImage();
                     android.graphics.Bitmap originalThermalImageBitmap = BitmapAndroid.createBitmap(javaBuffer).getBitMap();
                     imageView_markerImage.setImageBitmap(originalThermalImageBitmap);
                     imageOriginalDimensions = new int[]{originalThermalImageBitmap.getWidth(), originalThermalImageBitmap.getHeight()};
                 }
                 else {
-                    Log.e(TAG, "SetPicture: IS NOT A THERMAL PICTURE");
+                    Log.e("SetPicture", "IS NOT A THERMAL PICTURE");
                 }
             }
             catch (IOException e) {
-                Log.e(TAG, "setPicture: IO ERROR: " + e.toString());
-                //TODO: Handle IO exception
+                Log.e("setPicture", "IO ERROR: " + e.toString());
             }
         }
         else {
@@ -136,30 +101,31 @@ public class MarkerActivity extends AppCompatActivity {
     }
 
     private void addMarkers(int[] imageOriginalDimensions, int[] imageViewDimensions, int horizontalOffset) {
+        int markerWidthHeight = 200;
 
         RelativeLayout relativeLayout_markers = findViewById(R.id.relativeLayout_markers);
         ImageView imageView_eyeMarker = new ImageView(this);
         ImageView imageView_noseMarker = new ImageView(this);
 
-        imageView_eyeMarker.setImageURI(Uri.parse(eyeMarkerPath));
-        imageView_noseMarker.setImageURI(Uri.parse(noseMarkerPath));
+        imageView_eyeMarker.setImageURI(Uri.parse("android.resource://" + this.getPackageName() + "/drawable/eye_marker"));
+        imageView_noseMarker.setImageURI(Uri.parse("android.resource://" + this.getPackageName() + "/drawable/nose_marker"));
 
         SetOnTouchListener(imageView_noseMarker);
         SetOnTouchListener(imageView_eyeMarker);
 
         RelativeLayout.LayoutParams eyeParams = new RelativeLayout.LayoutParams(markerWidthHeight, markerWidthHeight);
         int[] scaledEyeMarkerPosition = Scaling.getScaledMarkerPosition(gradientAndPositions.getEyePosition(), imageOriginalDimensions, imageViewDimensions, horizontalOffset);
-        eyeParams.leftMargin = scaledEyeMarkerPosition[0] - markerWidthHeight/2;
-        eyeParams.topMargin = scaledEyeMarkerPosition[1] - markerWidthHeight/2;
+        eyeParams.leftMargin = scaledEyeMarkerPosition[0] - markerWidthHeight / 2;
+        eyeParams.topMargin = scaledEyeMarkerPosition[1] - markerWidthHeight / 2;
         relativeLayout_markers.addView(imageView_eyeMarker, eyeParams);
 
         RelativeLayout.LayoutParams noseParams = new RelativeLayout.LayoutParams(markerWidthHeight, markerWidthHeight);
         int[] scaledNoseMarkerPosition = Scaling.getScaledMarkerPosition(gradientAndPositions.getNosePosition(), imageOriginalDimensions, imageViewDimensions, horizontalOffset);
-        noseParams.leftMargin = scaledNoseMarkerPosition[0] - markerWidthHeight/2;
-        noseParams.topMargin = scaledNoseMarkerPosition[1] - markerWidthHeight/2;
+        noseParams.leftMargin = scaledNoseMarkerPosition[0] - markerWidthHeight / 2;
+        noseParams.topMargin = scaledNoseMarkerPosition[1] - markerWidthHeight / 2;
         relativeLayout_markers.addView(imageView_noseMarker, noseParams);
 
-        Log.e("TEST 2 (addMarkers)", "eye x: " + eyeParams.leftMargin + ", eye y: " + eyeParams.topMargin
+        Log.e("addMarkers", "eye x: " + eyeParams.leftMargin + ", eye y: " + eyeParams.topMargin
                 + ". nose x: " + noseParams.leftMargin + ", nose y: " + noseParams.topMargin
                 + ". imageView x: " + imageWidth + ", imageView y: " + imageHeight
                 + ". markerWidth: " + markerWidthHeight + ". horizontal offset: " + horizontalOffset);
@@ -174,19 +140,52 @@ public class MarkerActivity extends AppCompatActivity {
 
         int x = coordinates[0] + markerHorizontalOffset / 2;
         int y = coordinates[1] + markerVerticalOffset / 2 - imageViewVerticalOffset;
+        // TODO: Save new coordinate for eye/nose in gradientAndPositions object (remember to scale to original image dimensions before saving)
         Log.e(String.valueOf(marker.getTag()), "x: " + x + ", y: " + y);
         getPixelColor(x, y);
     }
 
     private void getPixelColor(int x, int y) {
-        Bitmap rootElementBitmap = ImageProcessing.loadBitmapFromView(container);
+        // TODO: Test marker precision on color_test.png
+        Bitmap rootElementBitmap = ImageProcessing.loadBitmapFromView(imageView_markerImage);
 
+        // Setting screen boundary programmatically
         x = x < 0 ? 0 : Math.min(x, imageWidth - 1);
         y = y < 0 ? 0 : Math.min(y, imageHeight - 1);
 
         int targetPixel = rootElementBitmap.getPixel(x, y);
         Log.e("Target pixel", "x: " + x + ", y: " + y);
+        // TODO: Calculate new gradient based on adjusted value for eye/nose and save in gradientAndPositions object
         Log.e("Pixel color", Color.red(targetPixel) + "," + Color.green(targetPixel) + "," + Color.blue(targetPixel));
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void SetOnTouchListener(ImageView imageView) {
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            PointF DownPT = new PointF(); // Record Mouse Position When Pressed Down
+            PointF StartPT = new PointF(); // Record Start Position of 'imageView'
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_MOVE:
+                        imageView.setX((int) (StartPT.x + event.getX() - DownPT.x));
+                        imageView.setY((int) (StartPT.y + event.getY() - DownPT.y));
+                        StartPT.set(imageView.getX(), imageView.getY());
+                        break;
+                    case MotionEvent.ACTION_DOWN:
+                        DownPT.set(event.getX(), event.getY());
+                        StartPT.set(imageView.getX(), imageView.getY());
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        getCoordinates(imageView);
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
     //region Navigation buttons
