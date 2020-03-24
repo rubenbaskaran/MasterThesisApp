85292a27eb7c86c2982e2d3d1da116074aa3671e
Ruben Baskaran <rbaskaran92@gmail.com>
2020-03-24 18:53:31 +0100

    added initial methods

--- app/src/main/java/rubenkarim/com/masterthesisapp/Algorithms/RgbThermalAlgorithm.java
+++ app/src/main/java/rubenkarim/com/masterthesisapp/Algorithms/RgbThermalAlgorithm.java
@@ -1,4 +1,25 @@
 package rubenkarim.com.masterthesisapp.Algorithms;
 
-public class RgbThermalAlgorithm {
+import android.graphics.Bitmap;
+
+import rubenkarim.com.masterthesisapp.Models.GradientModel;
+
+public class RgbThermalAlgorithm  extends AbstractAlgorithm{
+
+    public RgbThermalAlgorithm(Bitmap rgbImage, Bitmap thermalImage) {
+
+    }
+
+    private void findLandmarksOnRgbImage(){
+
+    }
+
+    private void mapLandmarksToThermalImage(){
+
+    }
+
+    @Override
+    public GradientModel getGradientAndPositions() {
+        return null;
+    }
 }
