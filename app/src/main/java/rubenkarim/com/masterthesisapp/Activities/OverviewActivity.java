dc208247fa35feef7ae184c8043e9e68c79b6001
Karim <Karmo>
2020-03-29 16:53:01 +0200

    finished cpr textfield

--- app/src/main/java/rubenkarim/com/masterthesisapp/Activities/OverviewActivity.java
+++ app/src/main/java/rubenkarim/com/masterthesisapp/Activities/OverviewActivity.java
@@ -3,6 +3,7 @@ package rubenkarim.com.masterthesisapp.Activities;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,6 +13,7 @@ import rubenkarim.com.masterthesisapp.Models.GradientModel;
 import rubenkarim.com.masterthesisapp.R;
 
 public class OverviewActivity extends AppCompatActivity {
+    private static final String TAG = OverviewActivity.class.getSimpleName();
     private ImageView imageView_patientImage;
     private String filename = "android.resource://rubenkarim.com.masterthesisapp/drawable/" + "default_picture";
     private Boolean isThermalCameraOn = false;
@@ -69,6 +71,8 @@ public class OverviewActivity extends AppCompatActivity {
     }
 
     public void saveOnClick(View view) {
+
+        String cpr = cprTextField.getText().toString();
 
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(intent);
