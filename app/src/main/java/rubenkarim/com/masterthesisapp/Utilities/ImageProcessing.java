011b93743c3d8965299e1bc57656270d59d4c2b1
Karim <Karmo>
2020-04-07 23:48:26 +0200

    refactoring og et spørgsmål til ruben

--- app/src/main/java/rubenkarim/com/masterthesisapp/Utilities/ImageProcessing.java
+++ app/src/main/java/rubenkarim/com/masterthesisapp/Utilities/ImageProcessing.java
@@ -86,7 +86,7 @@ public class ImageProcessing {
         b.recycle();
     }
 
-
+    //TODO: Remove if not used
     public static Bitmap loadBitmapFromView(View view) {
         //Define a bitmap with the same size as the view
         Bitmap returnedBitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
