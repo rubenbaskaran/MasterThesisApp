1e45baaa80a7ef0bfc4ff283a9efa44bdbcd242c
Ruben Baskaran <rbaskaran92@gmail.com>
2020-04-08 14:43:35 +0200

    optimized minmax algorithm

--- app/src/main/java/rubenkarim/com/masterthesisapp/Algorithms/MinMaxAlgorithm.java
+++ app/src/main/java/rubenkarim/com/masterthesisapp/Algorithms/MinMaxAlgorithm.java
@@ -3,13 +3,14 @@ package rubenkarim.com.masterthesisapp.Algorithms;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 
-import com.flir.thermalsdk.image.ImageFactory;
 import com.flir.thermalsdk.image.ThermalImageFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import rubenkarim.com.masterthesisapp.Models.RoiModel;
-import rubenkarim.com.masterthesisapp.Utilities.ImageProcessing;
 import rubenkarim.com.masterthesisapp.Utilities.Scaling;
 
 public class MinMaxAlgorithm extends AbstractAlgorithm {
@@ -26,7 +27,7 @@ public class MinMaxAlgorithm extends AbstractAlgorithm {
     private String mThermalImagePath;
     //endregion
 
-    public MinMaxAlgorithm(ThermalImageFile thermalImage, RoiModel leftEye, RoiModel rightEye, RoiModel nose, RoiModel cameraPreviewElement) throws IOException {
+    public MinMaxAlgorithm(ThermalImageFile thermalImage, RoiModel leftEye, RoiModel rightEye, RoiModel nose, RoiModel cameraPreviewElement) {
         capturedImageBitmap = thermalImage;
 
         int[] imageOriginalDimensions = new int[]{capturedImageBitmap.getWidth(), capturedImageBitmap.getHeight()};
@@ -66,31 +67,84 @@ public class MinMaxAlgorithm extends AbstractAlgorithm {
         int[] leftUpperCornerLocation = roiCircle.getUpperLeftCornerLocation();
         center = new int[]{leftUpperCornerLocation[0] + radius, leftUpperCornerLocation[1] + radius};
         int[] position = new int[2];
+        int groupSize = radius;
         double maxValue = 0;
-        double minValue = 765;
-        double colorSum;
+        double minValue = 255 * 3 * (groupSize * groupSize);
         int targetPixel;
+        Integer[] centerPointInPixelGroup = null;
         Bitmap bitmap = super.getBitmap(capturedImageBitmap);
-        for (int x = leftUpperCornerLocation[0]; x <= leftUpperCornerLocation[0] + width; x++) {
-            for (int y = leftUpperCornerLocation[1]; y <= leftUpperCornerLocation[1] + height; y++) {
-                if (isPixelInsideRoi(x, y)) {
-                    targetPixel = bitmap.getPixel(x, y);
-                    colorSum = Color.red(targetPixel) + Color.green(targetPixel) + Color.blue(targetPixel);
+
+        for (int x = leftUpperCornerLocation[0]; x <= leftUpperCornerLocation[0] + width; x += groupSize) {
+            for (int y = leftUpperCornerLocation[1]; y <= leftUpperCornerLocation[1] + height; y += groupSize) {
+
+                int colorSumInPixelGroup = 0;
+                ArrayList groupOfPixels = new ArrayList();
+                boolean isDirty = false;
+
+                for (int nestedX = x; nestedX < x + groupSize; nestedX++) {
+                    for (int nestedY = y; nestedY < y + groupSize; nestedY++) {
+                        if (isPixelInsideRoi(nestedX, nestedY)) {
+                            isDirty = true;
+                            targetPixel = bitmap.getPixel(nestedX, nestedY);
+                            int pixelColorSum = Color.red(targetPixel) + Color.green(targetPixel) + Color.blue(targetPixel);
+                            colorSumInPixelGroup += pixelColorSum;
+                            groupOfPixels.add(new Integer[]{pixelColorSum, nestedX, nestedY});
+                        }
+                    }
+                }
+
+                if(isDirty) {
+                    Comparator<Integer[]> cmp = new Comparator<Integer[]>() {
+                        @Override
+                        public int compare(Integer[] integer, Integer[] t1) {
+                            return integer[0].compareTo(t1[0]);
+                        }
+                    };
+
+                    centerPointInPixelGroup = category.equals("max") ?
+                            Collections.max(groupOfPixels, cmp) :
+                            Collections.min(groupOfPixels, cmp);
+
+                    colorSumInPixelGroup = colorSumInPixelGroup / (groupSize * groupSize);
 
                     if (category.equals("max")) {
-                        if (colorSum > maxValue) {
-                            maxValue = colorSum;
-                            position = new int[]{x, y};
+                        if (colorSumInPixelGroup > maxValue) {
+                            maxValue = colorSumInPixelGroup;
+                            position = new int[]{centerPointInPixelGroup[1], centerPointInPixelGroup[2]};
                         }
-                    } else {
-                        if (colorSum < minValue) {
-                            minValue = colorSum;
-                            position = new int[]{x, y};
+                    }
+                    else {
+                        if (colorSumInPixelGroup < minValue) {
+                            minValue = colorSumInPixelGroup;
+                            position = new int[]{centerPointInPixelGroup[1], centerPointInPixelGroup[2]};
                         }
                     }
                 }
             }
         }
+
+
+//        for (int x = leftUpperCornerLocation[0]; x <= leftUpperCornerLocation[0] + width; x++) {
+//            for (int y = leftUpperCornerLocation[1]; y <= leftUpperCornerLocation[1] + height; y++) {
+//                if (isPixelInsideRoi(x, y)) {
+//                    targetPixel = bitmap.getPixel(x, y);
+//                    colorSum = Color.red(targetPixel) + Color.green(targetPixel) + Color.blue(targetPixel);
+//
+//                    if (category.equals("max")) {
+//                        if (colorSum > maxValue) {
+//                            maxValue = colorSum;
+//                            position = new int[]{x, y};
+//                        }
+//                    } else {
+//                        if (colorSum < minValue) {
+//                            minValue = colorSum;
+//                            position = new int[]{x, y};
+//                        }
+//                    }
+//                }
+//            }
+//        }
+
 
         return position;
     }
