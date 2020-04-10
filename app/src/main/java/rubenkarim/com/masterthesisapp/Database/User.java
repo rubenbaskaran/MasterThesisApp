08b8ce78d486daad15ac559dde7cf0241aeefb22
Ruben Baskaran <rbaskaran92@gmail.com>
2020-04-10 14:52:21 +0200

    added annotation for making cpr unique for each row

--- app/src/main/java/rubenkarim/com/masterthesisapp/Database/User.java
+++ app/src/main/java/rubenkarim/com/masterthesisapp/Database/User.java
@@ -2,12 +2,17 @@ package rubenkarim.com.masterthesisapp.Database;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
-@Entity
+@Entity(indices = {@Index(value = {"cpr_number"},
+        unique = true)})
 public class User {
     @PrimaryKey
     public int uid;
+
+    @ColumnInfo(name = "cpr_number")
+    public String cprNumber;
 
     @ColumnInfo(name = "first_name")
     public String firstName;
