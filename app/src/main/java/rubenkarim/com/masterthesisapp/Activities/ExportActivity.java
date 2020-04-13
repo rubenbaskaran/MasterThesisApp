389b6763dccb4bb74da1a578bd305aa864dd924e
Ruben Baskaran <rbaskaran92@gmail.com>
2020-04-13 12:15:04 +0200

    added code for saving db data in csv file

--- app/src/main/java/rubenkarim/com/masterthesisapp/Activities/ExportActivity.java
+++ app/src/main/java/rubenkarim/com/masterthesisapp/Activities/ExportActivity.java
@@ -2,6 +2,7 @@ package rubenkarim.com.masterthesisapp.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.LinearLayout;
 
@@ -10,9 +11,15 @@ import com.google.android.material.snackbar.Snackbar;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 import androidx.appcompat.app.AppCompatActivity;
 import rubenkarim.com.masterthesisapp.Database.AppDatabase;
+import rubenkarim.com.masterthesisapp.Database.Entities.Observation;
+import rubenkarim.com.masterthesisapp.Database.Entities.Patient;
 import rubenkarim.com.masterthesisapp.R;
 
 public class ExportActivity extends AppCompatActivity {
@@ -26,32 +33,34 @@ public class ExportActivity extends AppCompatActivity {
         linearLayout_exportActivity = findViewById(R.id.linearLayout_ExportActivity);
     }
 
-    // TODO: Get data from db and save to csv
     public void createCsvOnClick(View view) {
-//        AppDatabase.databaseWriteExecutor.execute(() -> {
-//            AppDatabase db = AppDatabase.getDatabase(getApplicationContext());
-//            List<Patient> patients = db.patientDao().getAllPatients();
-//            List<Observation> observations = db.observationDao().getAllObservations();
-//
-//            Patient patient = new Patient();
-//            patient.cprNumber = cpr.isEmpty() ? "0123456789" : cpr;
-//            db.patientDao().insertPatient(patient);
-//            Patient person = db.patientDao().findPatientByCprNumber("0123456789");
-//        });
+        AppDatabase.databaseWriteExecutor.execute(() -> {
+            AppDatabase db = AppDatabase.getDatabase(getApplicationContext());
+            List<Patient> patients = db.patientDao().getAllPatients();
+            List<Observation> observations = db.observationDao().getAllObservations();
 
-//        String filepath = "masterthesisapp" + File.separator + "DateTimeNow" + ".csv";
-//        File file = new File(filepath);
-//        StringBuilder stringBuilder = new StringBuilder();
-//        stringBuilder.append("test");
-//
-//        try {
-//            FileWriter fileWriter = new FileWriter(file);
-//            fileWriter.append(stringBuilder);
-//            fileWriter.close();
-//        }
-//        catch (IOException e) {
-//            e.printStackTrace();
-//        }
+            File logDirectory = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), "/Masterthesisexports/");
+            boolean isDirectoryCreated = logDirectory.exists() || logDirectory.mkdirs();
+
+            if (isDirectoryCreated) {
+                String dateTime = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss").format(new Timestamp(System.currentTimeMillis()));
+                String filepath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getPath() + "/Masterthesisexports/output_" + dateTime + ".csv";
+                File file = new File(filepath);
+                StringBuilder stringBuilder = new StringBuilder();
+
+                // TODO: Iterate through all observations
+                stringBuilder.append(dateTime).append(",").append(patients.get(0).patientId).append(",").append(patients.get(0).cprNumber);
+
+                try {
+                    FileWriter fileWriter = new FileWriter(file);
+                    fileWriter.append(stringBuilder);
+                    fileWriter.close();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     public void clearDatabaseOnClick(View view) {
