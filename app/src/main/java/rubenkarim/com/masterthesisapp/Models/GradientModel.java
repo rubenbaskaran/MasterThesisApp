1bd9fb4ff858e010ab7baa275e02cf16c5066c0d
Ruben Baskaran <rbaskaran92@gmail.com>
2020-04-13 18:51:20 +0200

    expanded gradientmodel class with nose temp, eye temp and markersAdjusted boolean

--- app/src/main/java/rubenkarim/com/masterthesisapp/Models/GradientModel.java
+++ app/src/main/java/rubenkarim/com/masterthesisapp/Models/GradientModel.java
@@ -27,16 +27,40 @@ public class GradientModel implements Serializable {
         this.nosePosition = nosePosition;
     }
 
+    public double getEyeTemperature() {
+        return eyeTemperature;
+    }
+
+    public void setEyeTemperature(double eyeTemperature) {
+        this.eyeTemperature = eyeTemperature;
+    }
+
+    public double getNoseTemperature() {
+        return noseTemperature;
+    }
+
+    public void setNoseTemperature(double noseTemperature) {
+        this.noseTemperature = noseTemperature;
+    }
+
+    public boolean isMarkersAdjusted() {
+        return markersAdjusted;
+    }
+
+    public void setMarkersAdjusted(boolean markersAdjusted) {
+        this.markersAdjusted = markersAdjusted;
+    }
+
     private double Gradient;
     private int[] eyePosition;
     private int[] nosePosition;
+    private double eyeTemperature;
+    private double noseTemperature;
+    private boolean markersAdjusted = false;
 
     public GradientModel(double gradient, int[] eyePosition, int[] nosePosition) {
         Gradient = gradient;
         this.eyePosition = eyePosition;
         this.nosePosition = nosePosition;
-    }
-
-    public GradientModel() {
     }
 }
