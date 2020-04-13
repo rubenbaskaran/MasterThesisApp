90c4901341eab06cf1a71c9e40e6cc8d675501eb
Ruben Baskaran <rbaskaran92@gmail.com>
2020-04-13 15:09:38 +0200

    added show/hide loading animation code for export db IO operations

--- app/src/main/java/rubenkarim/com/masterthesisapp/Activities/ExportActivity.java
+++ app/src/main/java/rubenkarim/com/masterthesisapp/Activities/ExportActivity.java
@@ -1,10 +1,12 @@
 package rubenkarim.com.masterthesisapp.Activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -15,28 +17,35 @@ import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import rubenkarim.com.masterthesisapp.Database.AppDatabase;
 import rubenkarim.com.masterthesisapp.Database.Entities.Observation;
 import rubenkarim.com.masterthesisapp.R;
+import rubenkarim.com.masterthesisapp.Utilities.Animation;
 
 public class ExportActivity extends AppCompatActivity {
 
-    LinearLayout linearLayout_exportActivity;
+    private LinearLayout linearLayout_exportActivity;
+    private ProgressBar progressBar_exportProgressbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_export);
         linearLayout_exportActivity = findViewById(R.id.linearLayout_ExportActivity);
+        progressBar_exportProgressbar = findViewById(R.id.progressBar_exportLoadingAnimation);
     }
 
     public void createCsvOnClick(View view) {
         AppDatabase.databaseWriteExecutor.execute(() -> {
+            runOnUiThread(() -> Animation.showLoadingAnimation(progressBar_exportProgressbar, null, null));
+
             AppDatabase db = AppDatabase.getDatabase(getApplicationContext());
             List<Observation> observations = db.observationDao().getAllObservations();
 
             if (observations.isEmpty()) {
+                runOnUiThread(() -> Animation.hideLoadingAnimation(progressBar_exportProgressbar, null, null));
                 Snackbar.make(linearLayout_exportActivity, "Database is empty", Snackbar.LENGTH_SHORT).show();
                 return;
             }
@@ -46,7 +55,7 @@ public class ExportActivity extends AppCompatActivity {
 
             if (isDirectoryCreated) {
                 String dateTime = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss").format(new Timestamp(System.currentTimeMillis()));
-                String filepath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getPath() + "/Masterthesisexports/output_" + dateTime + ".csv";
+                String filepath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).getPath() + "/Masterthesisexports/" + dateTime + ".csv";
                 File file = new File(filepath);
                 StringBuilder stringBuilder = new StringBuilder();
 
@@ -79,12 +88,19 @@ public class ExportActivity extends AppCompatActivity {
                     fileWriter.close();
                 }
                 catch (IOException e) {
-                    e.printStackTrace();
+                    runOnUiThread(() -> Animation.hideLoadingAnimation(progressBar_exportProgressbar, null, null));
+                    Snackbar.make(linearLayout_exportActivity, "Couldn't write to csv file", Snackbar.LENGTH_SHORT).show();
+                    return;
                 }
             }
             else {
+                runOnUiThread(() -> Animation.hideLoadingAnimation(progressBar_exportProgressbar, null, null));
                 Snackbar.make(linearLayout_exportActivity, "Couldn't create export directory", Snackbar.LENGTH_SHORT).show();
+                return;
             }
+
+            runOnUiThread(() -> Animation.hideLoadingAnimation(progressBar_exportProgressbar, null, null));
+            Snackbar.make(linearLayout_exportActivity, "CSV file created successfully", Snackbar.LENGTH_SHORT).show();
         });
     }
 
@@ -110,25 +126,40 @@ public class ExportActivity extends AppCompatActivity {
     }
 
     public void clearDatabaseOnClick(View view) {
-        AppDatabase.databaseWriteExecutor.execute(() -> {
-            AppDatabase db = AppDatabase.getDatabase(getApplicationContext());
-            int numberOfObservations = db.observationDao().getAllObservations().size();
-            String outputMessageOnSuccess = "Removed: " + numberOfObservations + " observation(s)";
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Deleting database")
+                .setMessage("Are you sure?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        AppDatabase.databaseWriteExecutor.execute(() -> {
+                            runOnUiThread(() -> Animation.showLoadingAnimation(progressBar_exportProgressbar, null, null));
 
-            if (numberOfObservations == 0) {
-                Snackbar.make(linearLayout_exportActivity, "Database already empty", Snackbar.LENGTH_SHORT).show();
-            }
-            else {
-                db.observationDao().deleteAllObservations();
+                            AppDatabase db = AppDatabase.getDatabase(getApplicationContext());
+                            int numberOfObservations = db.observationDao().getAllObservations().size();
 
-                if (db.observationDao().getAllObservations().isEmpty()) {
-                    Snackbar.make(linearLayout_exportActivity, outputMessageOnSuccess, Snackbar.LENGTH_LONG).show();
-                }
-                else {
-                    Snackbar.make(linearLayout_exportActivity, "Couldn't clear database", Snackbar.LENGTH_LONG).show();
-                }
-            }
-        });
+                            if (numberOfObservations == 0) {
+                                runOnUiThread(() -> Animation.hideLoadingAnimation(progressBar_exportProgressbar, null, null));
+                                Snackbar.make(linearLayout_exportActivity, "Database already empty", Snackbar.LENGTH_SHORT).show();
+                            }
+                            else {
+                                db.observationDao().deleteAllObservations();
+
+                                if (db.observationDao().getAllObservations().isEmpty()) {
+                                    runOnUiThread(() -> Animation.hideLoadingAnimation(progressBar_exportProgressbar, null, null));
+                                    Snackbar.make(linearLayout_exportActivity, "Removed: " + numberOfObservations + " observation(s)", Snackbar.LENGTH_LONG).show();
+                                }
+                                else {
+                                    runOnUiThread(() -> Animation.hideLoadingAnimation(progressBar_exportProgressbar, null, null));
+                                    Snackbar.make(linearLayout_exportActivity, "Couldn't clear database", Snackbar.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 
     public void backOnClick(View view) {
