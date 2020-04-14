19f6008a907ac8e71d038de801610fb1377e9e5a
Karim <Karmo>
2020-04-14 12:42:06 +0200

    getScalingRoiObject function is tested

--- app/src/test/java/rubenkarim/com/masterthesisapp/Utilities/ScalingTest.java
+++ app/src/test/java/rubenkarim/com/masterthesisapp/Utilities/ScalingTest.java
@@ -10,13 +10,24 @@ class ScalingTest {
 
     @Test
     void getScaledRoiObject() {
+        //Testing scaling
         int[] upperleftcornerlocation = {1,2};
         RoiModel roiModel = Scaling.getScaledRoiObject(new RoiModel(upperleftcornerlocation,2, 2),1,1,0);
         assertArrayEquals(upperleftcornerlocation, roiModel.getUpperLeftCornerLocation(), "RoiModel should not be changed when scaling with one");
 
         upperleftcornerlocation = new int[] {2,2};
-        RoiModel roiModel = Scaling.getScaledRoiObject(new RoiModel(upperleftcornerlocation,2, 2),1,1,0);
+        roiModel = Scaling.getScaledRoiObject(new RoiModel(upperleftcornerlocation,2, 2),2,2,0);
+        assertArrayEquals(new int[] {4,4}, roiModel.getUpperLeftCornerLocation(), "scaling is not calculated correct");
 
+        //Testing Horizontal offset
+        upperleftcornerlocation = new int[] {2,2};
+        roiModel = Scaling.getScaledRoiObject(new RoiModel(upperleftcornerlocation,2, 2),1,1,1);
+        assertArrayEquals(new int[] {2,1}, roiModel.getUpperLeftCornerLocation(), "Horizontal offset is not subtracted from y-axis");
+
+        //Testing Horizontal offset and scaling
+        upperleftcornerlocation = new int[] {2,2};
+        roiModel = Scaling.getScaledRoiObject(new RoiModel(upperleftcornerlocation,2, 2),2,6,1);
+        assertArrayEquals(new int[] {4,6}, roiModel.getUpperLeftCornerLocation(), "scaling is not calculated correct with horizontalOffset");
     }
 
     @Test
