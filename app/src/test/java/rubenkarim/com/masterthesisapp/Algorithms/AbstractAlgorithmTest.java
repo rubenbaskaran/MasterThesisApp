a7456fe6023049538f6df3a832a1e1d973becbf0
Karim <Karmo>
2020-04-15 12:54:50 +0200

    deleted unused exceptions

---
+++ app/src/test/java/rubenkarim/com/masterthesisapp/Algorithms/AbstractAlgorithmTest.java
@@ -0,0 +1,16 @@
+package rubenkarim.com.masterthesisapp.Algorithms;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbstractAlgorithmTest {
+
+    @Test
+    void testCalculateGradient() {
+    }
+
+    @Test
+    void getBitmap() {
+    }
+}
